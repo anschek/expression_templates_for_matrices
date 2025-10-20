f5e706ad3f0c168a6ca42617f3cc4c82b15f619b
anschek <anschek@yandex.ru>
2025-10-20 15:22:08 +0300

    feat: add lazy operations for subtraction and multiplication by scalar

--- include/matrix_base.h
+++ include/matrix_base.h
@@ -10,17 +10,46 @@
 template<typename Derived, typename T>
 class MatrixExpr {
 public:
-	size_t rows() const { return static_cast<const Derived &>(*this).rows(); }
-	size_t cols() const { return static_cast<const Derived &>(*this).cols(); }
+	size_t rows() const;
+	size_t cols() const;
+	T operator()(size_t i, size_t j) const;
+	template<typename Other> AddExpr<Derived,Other,T> operator+(const MatrixExpr<Other, T> &other) const;
+	template<typename Other> SubExpr<Derived,Other,T> operator-(const MatrixExpr<Other, T> &other) const;
+	ScalarMultExpr<Derived,T> operator*(double alpha) const;
 
-	T operator()(size_t i, size_t j) const {
-		return static_cast<const Derived &>(*this)(i, j);
-	}
+};
 
-	template<typename Other>
-	auto operator+(const MatrixExpr<Other, T> &other) const {
+template<typename Derived, typename T>
+size_t MatrixExpr<Derived, T>::rows() const {
+	return static_cast<const Derived &>(*this).rows();
+}
+
+template<typename Derived, typename T>
+size_t MatrixExpr<Derived, T>::cols() const {
+	return static_cast<const Derived &>(*this).cols();
+}
+
+template<typename Derived, typename T>
+T MatrixExpr<Derived, T>::operator()(size_t i, size_t j) const {
+	return static_cast<const Derived &>(*this)(i, j);
+}
+
+template<typename Derived, typename T>
+template<typename Other>
+AddExpr<Derived,Other,T> MatrixExpr<Derived, T>::operator+(const MatrixExpr<Other, T> &other) const {
 		return AddExpr<Derived, Other, T>(static_cast<const Derived &>(*this),
 										static_cast<const Other &>(other));
-	}
-};
+}
+
+template<typename Derived, typename T>
+template<typename Other>
+SubExpr<Derived, Other, T> MatrixExpr<Derived, T>::operator-(const MatrixExpr<Other, T> &other) const {
+	return SubExpr<Derived, Other, T>(static_cast<const Derived &>(*this),
+								static_cast<const Other &>(other));
+}
+
+template<typename Derived, typename T>
+ScalarMultExpr<Derived, T> MatrixExpr<Derived, T>::operator*(double alpha) const {
+	return ScalarMultExpr<Derived, T>(static_cast<const Derived &>(*this), alpha);
+}
 #endif //EXPRESSION_TEMPLATES_FOR_MATRICES_BASE_H
--- include/matrix_ops.h
+++ include/matrix_ops.h
@@ -6,29 +6,109 @@
 #define EXPRESSION_TEMPLATES_FOR_MATRICES_OPS_H
 #include <iostream>
 
-template<typename Derived, typename T>
-class MatrixExpr;
-
-template<typename LHS, typename RHS, typename T>
-class AddExpr;
+template<typename Derived, typename T> class MatrixExpr;
 
 template<typename LHS, typename RHS, typename T>
 class AddExpr : public MatrixExpr<AddExpr<LHS, RHS, T>, T> {
 public:
-	AddExpr(const LHS &lhs, const RHS &rhs) : lhs_(lhs), rhs_(rhs) {
-	}
-
-	size_t rows() const { return lhs_.rows(); }
-	size_t cols() const { return lhs_.cols(); }
-
-	T operator()(size_t i, size_t j) const {
-		std::cout << "AddExpr::operator() -> computing (" << i << ", " << j << ")\n";
-		return lhs_(i, j) + rhs_(i, j);
-	}
+	AddExpr(const LHS &lhs, const RHS &rhs);
+	size_t rows() const;
+	size_t cols() const;
+	T operator()(size_t i, size_t j) const;
 
 private:
 	const LHS &lhs_;
 	const RHS &rhs_;
 };
+
+template<typename LHS, typename RHS, typename T>
+class SubExpr : public MatrixExpr<SubExpr<LHS, RHS, T>, T> {
+public:
+	SubExpr(const LHS &lhs, const RHS &rhs);
+	size_t rows() const;
+	size_t cols() const;
+	T operator()(size_t i, size_t j) const;
+
+private:
+	const LHS &lhs_;
+	const RHS &rhs_;
+};
+
+template<typename E, typename T>
+class ScalarMultExpr : public MatrixExpr<ScalarMultExpr<E, T>, T> {
+public:
+	ScalarMultExpr(const E &expr, T alpha);
+	size_t rows() const;
+	size_t cols() const;
+	T operator()(size_t i, size_t j) const;
+
+private:
+	const E &expr_;
+	const T alpha_;
+};
+
+template<typename LHS, typename RHS, typename T>
+AddExpr<LHS, RHS, T>::AddExpr(const LHS &lhs, const RHS &rhs) : lhs_(lhs), rhs_(rhs) {
+	if (lhs.rows() != rhs.rows() || lhs.cols() != rhs.cols())
+		throw std::invalid_argument("Matrix dimensions do not match");
+}
+
+template<typename LHS, typename RHS, typename T>
+size_t AddExpr<LHS, RHS, T>::rows() const {
+	return lhs_.rows();
+}
+
+template<typename LHS, typename RHS, typename T>
+size_t AddExpr<LHS, RHS, T>::cols() const {
+	return lhs_.cols();
+}
+
+template<typename LHS, typename RHS, typename T>
+T AddExpr<LHS, RHS, T>::operator()(size_t i, size_t j) const {
+	std::cout << "AddExpr::operator() -> computing (" << i << ", " << j << ")\n";
+	return lhs_(i, j) + rhs_(i, j);
+}
+
+template<typename LHS, typename RHS, typename T>
+SubExpr<LHS, RHS, T>::SubExpr(const LHS &lhs, const RHS &rhs): lhs_(lhs), rhs_(rhs) {
+	if (lhs.rows() != rhs.rows() || lhs.cols() != rhs.cols())
+		throw std::invalid_argument("Matrix dimensions do not match");
+}
+
+template<typename LHS, typename RHS, typename T>
+size_t SubExpr<LHS, RHS, T>::rows() const {
+	return lhs_.rows();
+}
+
+template<typename LHS, typename RHS, typename T>
+size_t SubExpr<LHS, RHS, T>::cols() const {
+	return lhs_.cols();
+}
+
+template<typename LHS, typename RHS, typename T>
+T SubExpr<LHS, RHS, T>::operator()(size_t i, size_t j) const {
+	std::cout << "SubExpr::operator() -> computing (" << i << ", " << j << ")\n";
+	return lhs_(i, j) - rhs_(i, j);
+}
+
+template<typename E, typename T>
+ScalarMultExpr<E, T>::ScalarMultExpr(const E &expr, T alpha): expr_(expr), alpha_(alpha) {
+}
+
+template<typename E, typename T>
+size_t ScalarMultExpr<E, T>::rows() const {
+	return expr_.rows();
+}
+
+template<typename E, typename T>
+size_t ScalarMultExpr<E, T>::cols() const {
+	return expr_.cols();
+}
+
+template<typename E, typename T>
+T ScalarMultExpr<E, T>::operator()(size_t i, size_t j) const {
+	std::cout << "ScalarMultExpr::operator() -> computing (" << i << ", " << j << ")\n";
+	return expr_(i, j) * alpha_;
+}
 
 #endif //EXPRESSION_TEMPLATES_FOR_MATRICES_OPS_H
